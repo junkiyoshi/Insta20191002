a4d02273eed45c8a5fe5e1553cf2c29114455f90
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2019-10-02 20:03:31 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,80 @@
+﻿#include "ofApp.h"	
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(239);
+	ofSetColor(39);
+	ofNoFill();
+	ofSetLineWidth(2);
+	ofEnableDepthTest();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	this->cam.begin();
+	ofRotateY(ofGetFrameNum() * 0.5);
+
+	for (int len = 50; len <= 400; len += 50) {
+
+		vector<glm::vec3> vertices;
+		for (int param = 0; param < 100; param++) {
+
+			auto location = glm::vec3(this->make_point(len, param), 0);
+			auto len = glm::length(location);
+
+			auto angle_x = ofMap(sin(len * 0.01 + ofGetFrameNum() * 0.01), -1, 1, -PI * 0.25, PI * 0.25);
+			auto angle_y = ofMap(sin(len * 0.01 + ofGetFrameNum() * 0.01), -1, 1, -PI * 0.25, PI * 0.25);
+
+			auto rotation_x = glm::rotate(glm::mat4(), angle_x, glm::vec3(1, 0, 0));
+			auto rotation_y = glm::rotate(glm::mat4(), angle_y, glm::vec3(0, 1, 0));
+			location = glm::vec4(location, 0) * rotation_y * rotation_x;
+
+			vertices.push_back(location);
+		}
+
+		ofBeginShape();
+		ofVertices(vertices);
+		ofEndShape(true);
+	}
+
+	this->cam.end();
+}
+
+//--------------------------------------------------------------
+glm::vec2 ofApp::make_point(int len, int param) {
+
+	param = param % 100;
+	if (param < 25) {
+
+		return glm::vec2(ofMap(param, 0, 25, -len * 0.5, len * 0.5), -len * 0.5);
+	}
+	else if (param < 50) {
+
+		return glm::vec2(len * 0.5, ofMap(param, 25, 50, -len * 0.5, len * 0.5));
+	}
+	else if (param < 75) {
+
+		return glm::vec2(ofMap(param, 50, 75, len * 0.5, -len * 0.5), len * 0.5);
+	}
+	else {
+
+		return glm::vec2(-len * 0.5, ofMap(param, 75, 100, len * 0.5, -len * 0.5));
+	}
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
